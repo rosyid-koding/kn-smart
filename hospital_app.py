00dac245fad2f31b061b3ba8c02c20174eaffbee
rosyid-koding <rosyid@kodingnext.com>
2026-06-03 10:38:40 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -1,1 +1,7 @@
+import streamlit as st
+import pandas as pd
+import pickle
+import os
+import numpy as py
 
+st.set_page_config(page_title="Smart Hospital Patient Navigator", page_icon="🏥", layout="wide")
